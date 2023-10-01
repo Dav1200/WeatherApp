1a09f6b1e4a55e2484412dc754a30a759bdd5441
Dav1200 <davinder.singh@city.ac.uk>
2023-10-01 11:45:39 +0100

    basic GUI setup for weather application

--- untitled/src/loader.java
+++ untitled/src/loader.java
@@ -1,4 +1,9 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class loader extends JFrame {
     loader() {
@@ -17,10 +22,49 @@ public class loader extends JFrame {
 
     }
 
-    public void addComponents() {
+    private void addComponents() {
+        //search field to search relative location
+        JTextField searchField = new JTextField();
+        searchField.setFont(new Font("MS Mincho",Font.BOLD, 24));
+        searchField.setBounds(15,15,450,40);
+        add(searchField);
+
+        //add search button
+        
+
+        JButton searchButton = new JButton(addImage("src/Images/search1.png",70,40));
+        searchButton.setBounds( 490,15,70,40);
+        add(searchButton);
+
+
+        JLabel cloudyIcon = new JLabel(addImage("src/Images/cloudy.png",300,290));
+        cloudyIcon.setBounds(0,100,600,290);
+        add(cloudyIcon);
+
+        JLabel temperatureText  = new JLabel("10 C");
+        temperatureText.setBounds(0,410,600,40);
+        temperatureText.setFont(new Font("Dialog",Font.BOLD,50));
+        temperatureText.setHorizontalAlignment(SwingConstants.CENTER);
+        add(temperatureText);
+
+
+
 
     }
 
+    private ImageIcon addImage(String relativePath, int width, int height) {
+        try {
+            BufferedImage originalImage = ImageIO.read(new File(relativePath));
+            //sacle the image to size
+            Image scaledImage = originalImage.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+
+            return new ImageIcon(scaledImage);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Could not load image");
+        return null;
+    }
 
     public static void main(String[] args) {
        SwingUtilities.invokeLater(new Runnable() {
