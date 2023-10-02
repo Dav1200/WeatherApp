8975727745ec7506d1615fa6d92e2c293425cd71
Dav1200 <davinder.singh@city.ac.uk>
2023-10-02 13:51:06 +0100

    Icons Updated

--- untitled/src/loader.java
+++ untitled/src/loader.java
@@ -19,7 +19,6 @@ public class loader extends JFrame {
         setSize(590, 800);
         setLocationRelativeTo(null);
         setLayout(null);
-
         setResizable(false);
         addComponents();
 
@@ -28,55 +27,55 @@ public class loader extends JFrame {
     private void addComponents() {
         //search field to search relative location
         JTextField searchField = new JTextField();
-        searchField.setFont(new Font("MS Mincho",Font.BOLD, 24));
-        searchField.setBounds(15,15,450,40);
+        searchField.setFont(new Font("MS Mincho", Font.BOLD, 24));
+        searchField.setBounds(15, 15, 450, 40);
         add(searchField);
 
         //add search button
 
-        JButton searchButton = new JButton(addImage("src/Images/search1.png",70,40));
-        searchButton.setBounds( 490,15,70,40);
+        JButton searchButton = new JButton(addImage("src/Images/search1.png", 70, 40));
+        searchButton.setBounds(490, 15, 70, 40);
         add(searchButton);
 
-JLabel city = new JLabel();
-city.setFont(new Font("Dialog",Font.BOLD,20));
-city.setBounds(100,100,100,100);
-add(city);
+        JLabel city = new JLabel();
+        city.setFont(new Font("Dialog", Font.BOLD, 20));
+        city.setBounds(100, 100, 100, 100);
+        add(city);
 
 
-        JLabel cloudyIcon = new JLabel(addImage("src/Images/cloudy.png",300,290));
-        cloudyIcon.setBounds(-10,90,600,290);
+        JLabel cloudyIcon = new JLabel(addImage("src/Images/cloudy.png", 300, 290));
+        cloudyIcon.setBounds(-10, 90, 600, 290);
         add(cloudyIcon);
 
-        JLabel temperatureText  = new JLabel("10 C");
-        temperatureText.setBounds(-10,400,600,40);
-        temperatureText.setFont(new Font("Dialog",Font.BOLD,40));
+        JLabel temperatureText = new JLabel("10 C");
+        temperatureText.setBounds(-10, 400, 600, 40);
+        temperatureText.setFont(new Font("Dialog", Font.BOLD, 40));
         temperatureText.setHorizontalAlignment(SwingConstants.CENTER);
         add(temperatureText);
 
 
         JLabel weatherDescription = new JLabel("Sunny");
-        weatherDescription.setBounds(-10,430, 600, 60);
-        weatherDescription.setFont(new Font("Dialog",Font.PLAIN,40));
+        weatherDescription.setBounds(-10, 430, 600, 60);
+        weatherDescription.setFont(new Font("Dialog", Font.PLAIN, 40));
         weatherDescription.setHorizontalAlignment(SwingConstants.CENTER);
         add((weatherDescription));
 
-        JLabel humidityImage = new JLabel(addImage("src/Images/humid.png",60,60));
-        humidityImage.setBounds(60,550,60,60);
+        JLabel humidityImage = new JLabel(addImage("src/Images/humid.png", 60, 60));
+        humidityImage.setBounds(60, 550, 60, 60);
         add(humidityImage);
 
         JLabel humidTxt = new JLabel("<html> <b> Humidity </b> 100%</html>");
-        humidTxt.setFont(new Font("Dialog",Font.PLAIN,20));
-        humidTxt.setBounds(135,555,100,60);
+        humidTxt.setFont(new Font("Dialog", Font.PLAIN, 20));
+        humidTxt.setBounds(135, 555, 100, 60);
         add(humidTxt);
 
-        JLabel windSpeedIcon = new JLabel(addImage("src/Images/windIcon.png",50,50));
-        windSpeedIcon.setBounds(340,550,60,60);
+        JLabel windSpeedIcon = new JLabel(addImage("src/Images/windIcon.png", 50, 50));
+        windSpeedIcon.setBounds(340, 550, 60, 60);
         add(windSpeedIcon);
 
         JLabel windspeedtxt = new JLabel("<html> <b> Windspeed </b> 5.2km/h</html>");
-        windspeedtxt.setFont(new Font("Dialog",Font.PLAIN,20));
-        windspeedtxt.setBounds(415,555,120,60);
+        windspeedtxt.setFont(new Font("Dialog", Font.PLAIN, 20));
+        windspeedtxt.setBounds(415, 555, 120, 60);
         add(windspeedtxt);
 
 
@@ -84,42 +83,37 @@ add(city);
             @Override
             public void actionPerformed(ActionEvent e) {
 
-            try
-            {
-                JSONObject data = backEndLogic.getWeatherData(searchField.getText().toString());
+                try {
+                    JSONObject data = backEndLogic.getWeatherData(searchField.getText().toString());
 
-                temperatureText.setText(data.get("temperature").toString());
-                windspeedtxt.setText("<html> <b> Windspeed </b>"+ data.get("windspeed").toString()+"km/h</html>");
-                humidTxt.setText("<html> <b> Humidity </b>"+ data.get("humidity")+ "%</html>");
-                weatherDescription.setText(data.get("weather_condition").toString());
+                    temperatureText.setText(data.get("temperature").toString());
+                    windspeedtxt.setText("<html> <b> Windspeed </b>" + data.get("windspeed").toString() + "km/h</html>");
+                    humidTxt.setText("<html> <b> Humidity </b>" + data.get("humidity") + "%</html>");
+                    weatherDescription.setText(data.get("weather_condition").toString());
 
-                if(data.get("weather_condition").equals("Clear")){
-                    cloudyIcon.setIcon(addImage("src/Images/sunny.png",300,290));
+                    if (data.get("weather_condition").equals("Clear")) {
+                        cloudyIcon.setIcon(addImage("src/Images/sunny.png", 300, 290));
 
-                } else if (data.get("weather_condition").equals("Cloudy")) {
-                    cloudyIcon.setIcon(addImage("src/Images/cloudy.png",300,290));
+                    } else if (data.get("weather_condition").equals("Cloudy")) {
+                        cloudyIcon.setIcon(addImage("src/Images/cloudy.png", 300, 290));
 
+                    } else if (data.get("weather_condition").equals("Rain")) {
+                        cloudyIcon.setIcon(addImage("src/Images/rain.png", 300, 290));
+
+                    } else if (data.get("weather_condition").equals("Snow")) {
+                        cloudyIcon.setIcon(addImage("src/Images/snow.png", 300, 290));
+
+                    }
+
+                } catch (Exception a) {
+                    JOptionPane.showMessageDialog(new JOptionPane(), "Enter Valid Location");
                 }
-                else if (data.get("weather_condition").equals("Rain")) {
-                    cloudyIcon.setIcon(addImage("src/Images/rain.png",300,290));
-
-                }
-                else if (data.get("weather_condition").equals("Snow")) {
-                    cloudyIcon.setIcon(addImage("src/Images/snow.png",300,290));
-
-                }
-
-            }catch (Exception a){
-                JOptionPane.showMessageDialog(new JOptionPane(),"Enter Valid Location");
-            }
 
             }
         });
 
 
         //add wind speed and direction.
-
-
 
 
     }
@@ -139,12 +133,12 @@ add(city);
     }
 
     public static void main(String[] args) {
-       SwingUtilities.invokeLater(new Runnable() {
-           @Override
-           public void run() {
-              new loader().setVisible(true);
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new loader().setVisible(true);
 
-           }
-       });
+            }
+        });
     }
 }
