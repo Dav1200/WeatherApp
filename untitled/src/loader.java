af0f03ff94d86ca43936ab07a456fccdfe8c1ab2
Dav1200 <davinder.singh@city.ac.uk>
2023-10-02 12:18:18 +0100

    GUI completed

--- untitled/src/loader.java
+++ untitled/src/loader.java
@@ -19,7 +19,6 @@ public class loader extends JFrame {
         setResizable(false);
         addComponents();
 
-
     }
 
     private void addComponents() {
@@ -41,13 +40,37 @@ public class loader extends JFrame {
         add(cloudyIcon);
 
         JLabel temperatureText  = new JLabel("10 C");
-        temperatureText.setBounds(0,410,600,40);
-        temperatureText.setFont(new Font("Dialog",Font.BOLD,50));
+        temperatureText.setBounds(0,380,600,40);
+        temperatureText.setFont(new Font("Dialog",Font.BOLD,40));
         temperatureText.setHorizontalAlignment(SwingConstants.CENTER);
         add(temperatureText);
 
-        //add description
-        //add humidity
+
+        JLabel weatherDescription = new JLabel("Sunny");
+        weatherDescription.setBounds(0,410, 600, 60);
+        weatherDescription.setFont(new Font("Dialog",Font.PLAIN,40));
+        weatherDescription.setHorizontalAlignment(SwingConstants.CENTER);
+        add((weatherDescription));
+
+        JLabel humidityImage = new JLabel(addImage("src/Images/humid.png",60,60));
+        humidityImage.setBounds(60,550,60,60);
+        add(humidityImage);
+
+        JLabel humidTxt = new JLabel("<html> <b> Humidity </b> 100%</html>");
+        humidTxt.setFont(new Font("Dialog",Font.PLAIN,20));
+        humidTxt.setBounds(135,555,100,60);
+        add(humidTxt);
+
+        JLabel windSpeedIcon = new JLabel(addImage("src/Images/windIcon.png",50,50));
+        windSpeedIcon.setBounds(340,550,60,60);
+        add(windSpeedIcon);
+
+        JLabel windspeedtxt = new JLabel("<html> <b> Windspeed </b> 5.2km/h</html>");
+        windspeedtxt.setFont(new Font("Dialog",Font.PLAIN,20));
+        windspeedtxt.setBounds(415,555,120,60);
+        add(windspeedtxt);
+
+    
         //add wind speed and direction.
 
 
